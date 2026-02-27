b1ad22eb7d7b757db86d0487348d469573e6d13f
Shen Tang <stang311@gatech.edu>
2026-02-26 19:28:01 -0500

    Added @OneToOne Bi-Directional Mapping project with cascading delete

---
+++ 02-jpa-one-to-one-bi/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,61 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public class AppDAOImpl implements AppDAO {
+    // define field for entity manager
+    private EntityManager entityManager;
+    // inject entity manager using constructor injection
+    @Autowired
+    public AppDAOImpl(EntityManager theEntityManager) {
+        this.entityManager = theEntityManager;
+    }
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        // This will ALSO save the details object,
+        // Because of CascadeType.ALL
+        entityManager.persist(theInstructor);
+    }
+    @Override
+    public Instructor findInstructorById(int theId) {
+        // This will ALSO retrieve the instructor details object
+        // Because of default behavior of @OneToOne fetch type is eager
+        return entityManager.find(Instructor.class, theId);
+    }
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theId) {
+        // retrieve the instructor
+        Instructor tempInstructor = entityManager.find(Instructor.class, theId);
+        // delete the instructor
+        if (tempInstructor != null) {
+            entityManager.remove(tempInstructor);
+        }
+    }
+    @Override
+    public InstructorDetail findInstructorDetailById(int theId) {
+        return entityManager.find(InstructorDetail.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int theId) {
+        // retrieve instructor detail
+        InstructorDetail tempInstructorDetail = entityManager.find(InstructorDetail.class, theId);
+        // remove the associated object reference
+        // break bi-directional link
+        //
+        tempInstructorDetail.getInstructor().setInstructorDetail(null);
+        // delete the instructor detail
+        entityManager.remove(tempInstructorDetail);
+    }
+}
